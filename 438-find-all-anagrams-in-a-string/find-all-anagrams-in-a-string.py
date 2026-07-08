989f54cb215ed29a3beeb59ddb45862018a0e0f5
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-07-09 00:13:36 +0530

    Update find-all-anagrams-in-a-string.py

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -27,3 +27,17 @@ class Solution:
                 ans.append(i - len(p) + 1)
 
         return ans
+
+""" ALTERNATE SOLUTION BUT USE OF COUNTER MAKES CODE BULKY AND SLOW(BUT IT'S SIMPLER)
+from collections import Counter
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        n = len(s)
+        m = len(p)
+        p_c = Counter(p)
+        ans = []
+        for i in range(n-m + 1):
+            if Counter(s[i:i+m]) == p_c:
+                ans.append(i)
+        return ans
+"""
