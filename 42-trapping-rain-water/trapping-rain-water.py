d20b4050de7af73914c9a629fa028955e1a93259
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-07-08 20:16:44 +0530

    Update trapping-rain-water.py

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -4,6 +4,7 @@ class Solution:
         suf = [0]*len(h)
         suf[-1] = h[-1]
         w=0
+        #ek banega prefix(left_max), ek banega suffix(right_max). Pani hoga beech me
         for i in range(1, len(h)):
             pre.append(max(pre[i-1], h[i]))
         
@@ -11,7 +12,7 @@ class Solution:
             suf[i] = max(suf[i + 1], h[i])
         
         for i in range(len(h)):
-            if h[i]<pre[i] and h[i]<suf[i]:
+            if h[i]<pre[i] and h[i]<suf[i]:  #ye bahut important
                 w+=min(pre[i], suf[i])-h[i]
 
         return w
